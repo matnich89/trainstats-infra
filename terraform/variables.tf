fa9fc2d3bd85828f55d3c85bfacb655ebb775c76
mathewnicholls <mathewnicholls@protonmail.com>
2024-10-13 11:02:06 +0100

    fix formatting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,14 @@
-variable vpc_cidr_block {}
-variable private_subnet_cidr_blocks {}
-variable public_subnet_cidr_blocks {}
+variable "vpc_cidr_block" {
+  type        = string
+  description = "The CIDR block for the VPC"
+}
+
+variable "private_subnet_cidr_blocks" {
+  type        = list(string)
+  description = "List of CIDR blocks for private subnets"
+}
+
+variable "public_subnet_cidr_blocks" {
+  type        = list(string)
+  description = "List of CIDR blocks for public subnets"
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,17 +1,15 @@
-
-
 module "trainstats-vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
 
-  name = "trainstats-vpc"
-  cidr = var.vpc_cidr_block
+  name            = "trainstats-vpc"
+  cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
-  public_subnets = var.public_subnet_cidr_blocks
-  azs = ["us-east-1a"]
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = ["us-east-1a"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -20,12 +18,11 @@ module "trainstats-vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/trainstats-eks-cluster" = "shared"
-    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/role/elb"                       = 1
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/trainstats-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/internal-elb"              = 1
   }
-
 }
