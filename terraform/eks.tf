66e10dbcca4670a60f99477b99214c84e05eea9d
mathewnicholls <mathewnicholls@protonmail.com>
2024-10-16 09:22:37 +0100

    format

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,26 +2,24 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.26.0"
 
-  cluster_name = "trainstats-eks-cluster"
+  cluster_name    = "trainstats-eks-cluster"
   cluster_version = "1.30"
 
   subnet_ids = module.trainstats-vpc.private_subnets
-  vpc_id = module.trainstats-vpc.vpc_id
+  vpc_id     = module.trainstats-vpc.vpc_id
 
   tags = {
-    environment  = "production"
+    environment = "production"
     application = "trainstats"
   }
 
   eks_managed_node_groups = {
     prod = {
-      min_size = 1
-      max_size = 3
+      min_size     = 1
+      max_size     = 3
       desired_size = 3
 
       instance_types = ["t3.small"]
-
     }
   }
-
 }
