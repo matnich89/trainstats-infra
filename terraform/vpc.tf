6ba22ecd59f052d1b8f57bb7da767f1127526841
mathewnicholls <mathewnicholls@protonmail.com>
2024-10-16 14:26:48 +0100

    move to digital oceon

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,28 +1,6 @@
-module "trainstats-vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.13.0"
 
-  name            = "trainstats-vpc"
-  cidr            = var.vpc_cidr_block
-  private_subnets = var.private_subnet_cidr_blocks
-  public_subnets  = var.public_subnet_cidr_blocks
-  azs             = ["us-east-1a", "us-east-1b"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    "kubernetes.io/cluster/trainstats-eks-cluster" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/trainstats-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"                       = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/trainstats-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"              = 1
-  }
+resource "digitalocean_vpc" "k8s_vpc" {
+  name     = "k8s-vpc-ams"
+  region   = "ams3"  # Amsterdam region
+  ip_range = "10.10.0.0/16"
 }
