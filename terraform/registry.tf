5f023fefb9929d4f1aa134773444314a3f82c295
mathewnicholls <mathewnicholls@protonmail.com>
2024-10-16 14:49:44 +0100

    fix registries

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,4 +1,4 @@
 resource "digitalocean_container_registry" "image-registry" {
-  name                   = "image-registry"
+  name                   = "mat-n-image-registry"
   subscription_tier_slug = "basic"
 }
